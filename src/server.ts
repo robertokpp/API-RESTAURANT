15e3adbb31f45b5daeac65807b4bb6c4364cd25e
Roberto Kennedy <roberto.kpp@gmail.com>
2026-01-29 22:02:50 -0500

    feat: adicionar configuração de scripts e melhorar a estrutura do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
-import express from "express"
+import express from "express";
 
-const port = 3333
-const app = express()
+const PORT = 3333;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
